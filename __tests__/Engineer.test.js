9fda17d02cc1ca836c38e842112ce71b0b349ccf
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2021-12-11 23:18:07 -0800

    successful tests for Engineer.js

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,18 @@
+const Engineer = require('../lib/Engineer');
+
+test('check if github can be passed from contructor argument', () => {
+    const employee = new Engineer('Ken','123456', 'kentest@xyz.com', 'khanhlam90');
+    expect(employee.github).toBe('khanhlam90');
+});
+
+test('check if can get github from getGithub()', () => {
+    const employee = new Engineer('Ken','123456', 'kentest@xyz.com', 'khanhlam90');
+    expect(employee.getGithub()).toBe('khanhlam90');
+});
+
+test('check if can get Engineer role from getRole()', () => {
+    const employee = new Engineer('Ken','123456', 'kentest@xyz.com');
+    expect(employee.getRole()).toBe('Engineer');
+});
+
+
