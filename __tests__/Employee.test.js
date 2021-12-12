7ef996c3da42cf35885845c678a08312fbc40bff
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2021-12-11 23:01:31 -0800

    successful Employee() tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,41 @@
-const Employee = require('../lib/Employee.js');
+const Employee = require('../lib/Employee');
 
-test('to check if the employee data export as expected', () => {
-    const employee = new Employee('Sun', 'F12365', 'xyz@gmail.com');
+test('to check if Employee could create object', () => {
+    const employee = new Employee();
+    expect(employee).toEqual(expect.any(Object));
+});
 
-    expect(employee.name).toBe('Sun');
-    expect(employee.id).toEqual(expect.any(String));
-    expect(employee.email).toEqual(expect.any(String));
+test('check if name can be passed from contructor argument', () => {
+    const employee = new Employee('Ken');
+    expect(employee.name).toBe('Ken');
+});
+
+test('check if id can be passed from contructor argument', () => {
+    const employee = new Employee('Ken','123456');
+    expect(employee.id).toBe('123456');
+});
+
+test('check if email can be passed from contructor argument', () => {
+    const employee = new Employee('Ken','123456', 'kentest@xyz.com');
+    expect(employee.email).toBe('kentest@xyz.com');
+});
+
+test('check if can get name from getName()', () => {
+    const employee = new Employee('Ken');
+    expect(employee.getName()).toBe('Ken');
+});
+
+test('check if can get id from getId()', () => {
+    const employee = new Employee('Ken','123456');
+    expect(employee.getId()).toBe('123456');
+});
+
+test('check if can get email from getEmail()', () => {
+    const employee = new Employee('Ken','123456', 'kentest@xyz.com');
+    expect(employee.getEmail()).toBe('kentest@xyz.com');
+});
+
+test('check if can get default role of Employee from getRole()', () => {
+    const employee = new Employee('Ken','123456', 'kentest@xyz.com');
+    expect(employee.getRole()).toBe('Employee');
 });
