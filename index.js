c02ad9f3f9be59a002b3fe68aaa6cdd30090e205
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2021-12-12 16:35:07 -0800

    finished html content and create/copy files

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const inquirer = require ('inquirer');
+//const stylesheet = require ('./src/css');
 const fs = require ('fs');
 
 const Employee = require ('./lib/Employee');
 const Manager = require ('./lib/Manager');
 const Engineer = require ('./lib/Engineer');
 const Intern = require ('./lib/Intern');
+const { SIGTERM } = require('constants');
 
 let finalTeamObj = [];
 
-function userPrompt() {
+// prompt user's input for team name
+function promptUser() {
     inquirer
     .prompt ([
         {
@@ -28,10 +31,13 @@ function userPrompt() {
     .then (function (teamNameData) {
         const teamName = teamNameData.teamname;
         finalTeamObj.push(teamName);
+
+        //after entering the team name, call fxn to enter manager's data
         addManager();
     });
 };
 
+// prompt user's input for team manager's data
 function addManager() {
     inquirer
     .prompt ([
@@ -92,15 +98,17 @@ function addManager() {
         const managerName = managerData.name;
         const managerId = managerData.id;
         const managerEmail = managerData.email;
-        const managerOfficeNumber = managerData.number;
+        const managerOfficeNumber = managerData.officeNumber;
         const teamMember = new Manager(managerName, managerId, managerEmail, managerOfficeNumber);
         finalTeamObj.push(teamMember);
 
+        // after manager's data input, call fxn to ask user if they wanted to add more team members
         addTeamMember();
 
     });
 };
 
+// prompt user's selection to add more team members of engineer and/or intern
 function addTeamMember() {
     inquirer
     .prompt ([
@@ -108,26 +116,30 @@ function addTeamMember() {
             type: 'list',
             name: 'addMember',
             message: 'Would you like to add team members?',
-            choices: ['Yes, add an engineer','Yes, add an intern','No, continue processing team profile'],
+            choices: ['Yes, add an engineer','Yes, add an intern','No, generate our Team Profile'],
         },
     ])
     .then(function(addMemberData) {
         switch (addMemberData.addMember) {
             case 'Yes, add an engineer':
+                // call engineer if user decided to add engineer member
                 addEngineer();
                 break;
             
             case 'Yes, add an intern':
+                // call intern if user decided to add intern member
                 addIntern();
                 break;
 
-            case 'No, continue processing team profile':
+            case 'No, generate our Team Profile':
+                // if no more member addition, call fxn to create html
                 pageTemplate();
                 break;  
         }
     });
 };
 
+// prompt user's input for team engineer's data
 function addEngineer() {
     inquirer
     .prompt ([
@@ -192,11 +204,13 @@ function addEngineer() {
         const teamMember = new Engineer(engineerName, engineerId, engineerEmail, engineerGithub);
         finalTeamObj.push(teamMember);
 
+        // continue calling fxn to ask user if they wanted to add more team members
         addTeamMember();
 
     });
 };
 
+// prompt user's input for team intern's data
 function addIntern() {
     inquirer
     .prompt ([
@@ -261,11 +275,120 @@ function addIntern() {
         const teamMember = new Intern(internName, internId, internEmail, internSchool);
         finalTeamObj.push(teamMember);
 
+        // continue calling fxn to ask user if they wanted to add more team members
         addTeamMember();
 
     });
 };
 
+// after no more team member needed to be added, start to construct html content
 function pageTemplate() {
+
+    //this is the final of the html content, set as an array as more objects will be pushed in here:
+    const htmlFinalObj = [];
+
+    // this is the opening and header of the html
+    const htmlHeader = `
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>${finalTeamObj[0]}</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+   
+    <body>
+        <header>
+            <h1>${finalTeamObj[0]}</h1>
+        </header>
+
+        <div class = "card-container">
+    `;
+    // push the current data to htmlFinalObj
+    htmlFinalObj.push(htmlHeader);
+
+    //performing the for loop to construct the html body
+    for (let i = 1; i<finalTeamObj.length; i++) {
+        let htmlCurrentObj = `
+            <div class = "card-member"> 
+                <div class = "card-header">
+                    <h2>${finalTeamObj[i].name}</h2>
+                    <h3>${finalTeamObj[i].role}</h3>
+                </div>
+                <div class = "card-body">
+                    <p>Employee ID: ${finalTeamObj[i].id}</p>
+                    <p>Email: <a href = "mailto: ${finalTeamObj[i].email}">${finalTeamObj[i].email}</a></p>
+            `
+        // if fn to check if member has office number
+        if (finalTeamObj[i].officeNumber) {
+            htmlCurrentObj += `
+            <p>Office Number: ${finalTeamObj[i].officeNumber}</p>
+            `
+        }
+        // if fn to check if member has Github username
+        if (finalTeamObj[i].github) {
+            htmlCurrentObj += `
+            <p>Github: <a href = "https://github.com/${finalTeamObj[i].github}">${finalTeamObj[i].github}</a></p>
+            ` 
+        }
+        // if fn to check if member has school
+        if (finalTeamObj[i].school) {
+            htmlCurrentObj += `
+            <p>School: ${finalTeamObj[i].school}</p>
+            `
+        }
+
+        // add the closed div to current html
+        htmlCurrentObj += `
+        </div>
+        </div>
+        `
+        //push the current html content to the final html object
+        htmlFinalObj.push(htmlCurrentObj);
+    }
+
+    // add the closed div/body/html elements to the end of the html file
+    const htmlClosedElements = `
+    </div>
+    </body>
+    </html>
+    `
+    // push these closed elements to the final html object
+    htmlFinalObj.push(htmlClosedElements);
+
+    fs.writeFile(`./dist/${finalTeamObj[0]}.html`, htmlFinalObj.join(""), err => {
+        // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+        if (err) {
+            reject(err);
+            // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+            return;
+          }
     
-}
+          // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+          resolve({
+            ok: true,
+            message: 'Files created! Please check directory "dist" for final Team Profile HTML and related CSS!'
+        });
+    });
+
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+        // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+        if (err) {
+            reject(err);
+            // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+            return;
+        }
+
+        // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+        resolve({
+            ok: true,
+            message: 'Stylesheet created!'
+        });
+    });
+};
+
+
+promptUser();
